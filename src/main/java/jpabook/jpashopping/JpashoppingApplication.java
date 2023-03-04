5ccdc4de91ccbc3ebf15918313f2c727390f5ac5
Park JunHyuk <junni802@naver.com>
2023-03-04 16:39:49 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashopping/JpashoppingApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpashoppingApplication.class, args);
+    }
+
+}
